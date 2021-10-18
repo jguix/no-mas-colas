dea491aec2baac60dc8dd6a242efd89ac33d40bd
_jordaj3 <juangui.jordan2@takeda.com>
2021-10-18 02:31:52 +0200

    Basic starter with typescript config

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Home</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
